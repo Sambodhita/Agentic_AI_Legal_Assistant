9420cbd4ede847260dae20a96415d80df94b56fe
Sambodhita <144583765+Sambodhita@users.noreply.github.com>
2026-09-08 23:23:07 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -18,7 +18,7 @@ if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY is not configured.")
 
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
+    model="openai/gpt-oss-20b",
     temperature=0,
     groq_api_key=GROQ_API_KEY
 )
